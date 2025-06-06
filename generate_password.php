ef13ece6d1a287c340571af206ac6a81a6171c2e
nausamah27 <nausamah2711@gmail.com>
2025-06-07 00:09:10 +0700

    Update generate_password.php

--- generate_password.php
+++ generate_password.php
@@ -1,6 +1,6 @@
 <?php
-// Generate password hash for admin112
-$password = 'admin112';
+// Generate password hash for admin123
+$password = 'admin123';
 $hash = password_hash($password, PASSWORD_DEFAULT);
 echo "Password: " . $password . "<br>";
 echo "Hash: " . $hash . "<br>";
